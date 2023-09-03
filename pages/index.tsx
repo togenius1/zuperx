c5f3d506459d3948514605bd73668814af659756
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-03 22:45:23 +0700

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ import { Fragment } from 'react';
 import Head from 'next/head';
 
 import HomeBG from '../components/home-page/home-bg';
-// import homeImageUrl from '../public/images/home.png';
-// import btnGoogleUrl from '../public/images/btn_google.png';
-// import btnAppleUrl from '../public/images/btn_apple.png';
 
 export default function HomePage() {
     return (
