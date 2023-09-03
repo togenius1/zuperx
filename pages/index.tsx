5338b03c2077bf9db2c1494d75193be40a63471a
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-03 15:42:59 +0700

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             >
               By{' '}
               <Image
-                src="/vercel.svg"
+                src="/images/vercel.svg"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
