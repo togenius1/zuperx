de45d6132c5b343cf4309519ea4ef2ff994e87c8
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-03 20:56:20 +0700

    updated

--- components/home-page/home-bg.tsx
+++ components/home-page/home-bg.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import classes from './home.module.css';
 import ButtonGoogle from '../ui/btn-google';
 import ButtonApple from '../ui/btn-apple';
-import homeUrl from '/public/images/next.svg';
+import homeUrl from '/public/images/HOME.png';
 
 function HomeBG() {
     // const homeUrl =
