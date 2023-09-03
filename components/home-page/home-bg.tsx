a8220118043b7548db9962a5e7ff7df39e0f1862
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-03 21:43:15 +0700

    updated

--- components/home-page/home-bg.tsx
+++ components/home-page/home-bg.tsx
@@ -2,14 +2,13 @@
 import Image from 'next/image';
 
 import classes from './home.module.css';
-import styles from '@/styles/Home.module.css';
 import ButtonGoogle from '../ui/btn-google';
 import ButtonApple from '../ui/btn-apple';
-import homeUrl from '/public/images/test.jpeg';
+// import homeUrl from '/public/images/home.png';
 
 function HomeBG() {
-    // const homeUrl =
-    //     'https://zuperx-storage-0648618673334-main.s3.ap-southeast-1.amazonaws.com/public/images/home.png';
+    const homeUrl =
+        'https://zuperx-storage-0648618673334-main.s3.ap-southeast-1.amazonaws.com/public/images/home.png';
     // const homeUrl = '/images/home.png';
     const intrinsicWidth = 3600;
     const intrinsicHeight = 1800;
