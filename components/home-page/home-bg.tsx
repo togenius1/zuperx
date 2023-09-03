ff52b23b50301bead92fbd600f7d9ffa14065836
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-03 18:34:53 +0700

    png

--- components/home-page/home-bg.tsx
+++ components/home-page/home-bg.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 import classes from './home.module.css';
 import ButtonGoogle from '../ui/btn-google';
 import ButtonApple from '../ui/btn-apple';
-import homeImg from '../../public/images/home.png';
+import homeImg from '/public/images/home.png';
 
 function HomeBG() {
-    // const homeUrl =
-    //     'https://zuperx-storage-0648618673334-main.s3.ap-southeast-1.amazonaws.com/public/images/home.png';
-    const homeUrl = '/images/home.png';
+    const homeUrl =
+        'https://zuperx-storage-0648618673334-main.s3.ap-southeast-1.amazonaws.com/public/images/home.png';
+    // const homeUrl = '/images/home.png';
     const intrinsicWidth = 3600;
     const intrinsicHeight = 1800;
 
