2ce45b1453e40cc62b226235bf3b432554d9b08f
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-03 19:59:00 +0700

    updated

--- components/home-page/home-bg.tsx
+++ components/home-page/home-bg.tsx
@@ -3,34 +3,23 @@ import Image from 'next/image';
 import classes from './home.module.css';
 import ButtonGoogle from '../ui/btn-google';
 import ButtonApple from '../ui/btn-apple';
-import homeImg from '/public/images/home.png';
+import homeImg from '/public/images/home.jpg';
 
 function HomeBG() {
-    const homeUrl =
-        'https://zuperx-storage-0648618673334-main.s3.ap-southeast-1.amazonaws.com/public/images/home.png';
-    // const homeUrl = '/images/home.png';
-    const intrinsicWidth = 3600;
-    const intrinsicHeight = 1800;
+    // const homeUrl =
+    //     'https://zuperx-storage-0648618673334-main.s3.ap-southeast-1.amazonaws.com/public/images/home.png';
+    const homeUrl = '/images/home.png';
+    // const intrinsicWidth = 3600;
+    // const intrinsicHeight = 1800;
 
     return (
         <section className={classes.home}>
             <div className={classes.image}>
-                {/* <Image
-                    src={homeImg}
-                    alt='Finaci, Money manager'
-                    width={intrinsicWidth}
-                    height={intrinsicHeight}
-                    style={{
-                        height: 'auto',
-                        width: '100%',
-                        // objectFit: 'cover',
-                    }}
-                    priority
-                /> */}
                 <Image
-                    alt='Mountains'
+                    alt='Finaci, Money manager'
                     src={homeImg}
                     placeholder='blur'
+                    priority
                     quality={100}
                     fill
                     sizes='100vw'
