912d439e8e484c9b96797e98d6be4c2f0c0301c6
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-04 22:18:24 +0700

    api

--- helpers/db-util.tsx
+++ helpers/db-util.tsx
@@ -1,12 +1,22 @@
 import { DataStore } from 'aws-amplify';
+import { API } from 'aws-amplify';
 
 import { Contact } from '@/src/models';
+import { createContact } from '@/src/graphql/mutations';
 
 // Insert comments
 export async function insertContact(newContact: ContactType) {
     try {
-        const insertedContact = await DataStore.save(new Contact(newContact));
-
+        const insertedContact = await API.graphql({
+            query: createContact,
+            variables: {
+                input: {
+                    email: newContact?.email,
+                    name: newContact?.name,
+                    message: newContact?.message,
+                },
+            },
+        });
         return insertedContact;
     } catch (error) {
         console.log('Error saving Contact', error);
