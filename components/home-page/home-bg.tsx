a7dab81ea2342e19cb65df9470617e3db184e00d
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-06 23:09:53 +0700

    updated

--- components/home-page/home-bg.tsx
+++ components/home-page/home-bg.tsx
@@ -26,9 +26,9 @@ function HomeBG() {
                         height: 'auto',
                     }}
                 />
-        
-                    <ButtonGoogle />
-             
+
+                <ButtonGoogle />
+
                 <ButtonApple />
             </div>
 
