29b64b83a5d7c2b2ac3c65e06fae0819a91b35c3
togenius1 <90688382+togenius1@users.noreply.github.com>
2023-09-07 19:59:52 +0700

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import Layout from '../components/layout/layout';
-import awsconfig from '../src/aws-exports';
+// import awsconfig from '../src/aws-exports';
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
